d3e88fe188050f26b57d041f0f7f3f4921c0a51b
Jianan Gao <ykgaojianan@gmail.com>
2019-10-02 20:59:17 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          changed
+          changed again
         </p>
         <a
           className="App-link"
